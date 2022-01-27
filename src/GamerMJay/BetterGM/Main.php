3f26bb94d69624252a3735e862447ee96b8fc1ac
GamerMJay <66413999+GamerMJay2008@users.noreply.github.com>
2022-01-28 00:59:44 +0100

    Update Main.php

--- src/GamerMJay/BetterGM/Main.php
+++ src/GamerMJay/BetterGM/Main.php
@@ -1,53 +1,37 @@
 <?php
 
+
+
 namespace GamerMJay\BetterGM;
 
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\player\GameMode;
-use pocketmine\player\Player;
-use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase
-{
-    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
-    {
-        if ($cmd->getName() === "gm0") {
-            if (!$sender->hasPermission("gm0.use")) {
-                $sender->sendMessage("§cYou do not have permission to use this command!");
-                return false;
-            }
-            $sender->setGamemode(GameMode::SURVIVAL());
-            $sender->sendMessage("§6§lBetterGameMode §r§8» §aYour gamemode has been changed to Survival");
-            return false;
-        }
-        if ($cmd->getName() === "gm1") {
-            if (!$sender->hasPermission("gm1.use")) {
-                $sender->sendMessage("§cYou do not have permission to use this command!");
-                return false;
-            }
-            $sender->setGamemode(GameMode::CREATIVE());
-            $sender->sendMessage("§6§lBetterGameMode §r§8» §aYour gamemode has been changed to Creative");
-            return false;
-        }
-        if ($cmd->getName() === "gm2") {
-            if (!$sender->hasPermission("gm2.use")) {
-                $sender->sendMessage("§cYou do not have permission to use this command!");
-                return false;
-            }
-            $sender->setGamemode(GameMode::ADVENTURE());
-            $sender->sendMessage("§6§lBetterGameMode §r§8» §aYour gamemode has been changed to Adventure");
-            return false;
-        }
-        if ($cmd->getName() === "gm3") {
-            if (!$sender->hasPermission("gm3.use")) {
-                $sender->sendMessage("§cYou do not have permission to use this command!");
-                return false;
-            }
-            $sender->setGamemode(GameMode::SPECTATOR());
-            $sender->sendMessage("§6§lBetterGameMode §r§8» §aYour gamemode has been changed to Spectator");
-            return false;
-        }
-        return true;
+
+use GamerMJay\BetterGM\commands\gm0;
+use GamerMJay\BetterGM\commands\gm1;
+use GamerMJay\BetterGM\commands\gm2;
+use GamerMJay\BetterGM\commands\gm3;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+
+
+class Main extends PluginBase {
+
+    public function onEnable(): void {
+
+        $this->saveResource('config.yml');
+
+        Server::getInstance()->getCommandMap()->registerAll('BetterGM',
+
+            [
+                 new gm0(),
+
+                new gm1(),
+
+                new gm2(),
+
+                new gm3(),
+
+            ]);
+
     }
 }
