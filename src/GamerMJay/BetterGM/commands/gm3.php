cfcbcfecfc2a1f5aefa1bf72cd866c95ea3018a0
GamerMJay <66413999+GamerMJay2008@users.noreply.github.com>
2022-01-28 00:52:26 +0100

    Create gm3.php

---
+++ src/GamerMJay/BetterGM/commands/gm3.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GamerMJay\BetterGM\commands;
+
+use GamerMJay\BetterGM\Main;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\GameMode;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase
+{
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool 
+    {
+        if ($cmd->getName() === "gm3") {
+            if (!$sender->hasPermission("gm3.use")) {
+                $sender->sendMessage("§cYou do not have permission to use this command!");
+                return false;
+            }
+            $sender->setGamemode(GameMode:: SPECTATOR());
+            $message = $config->get("gm3");
+            return false;
+        }
+        
+        return true;
+    }
+}
