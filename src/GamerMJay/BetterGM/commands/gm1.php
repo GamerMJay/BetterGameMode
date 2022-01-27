20dff15de62d8b3842efb24cfcdc00673e119162
GamerMJay <66413999+GamerMJay2008@users.noreply.github.com>
2022-01-28 00:46:31 +0100

    Create gm1.php

---
+++ src/GamerMJay/BetterGM/commands/gm1.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GamerMJay\BetterGM\commands;
+
+use GamerMJay\BetterGM\Main;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\GameMode;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase
+{
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool 
+    {
+        if ($cmd->getName() === "gm1") {
+            if (!$sender->hasPermission("gm1.use")) {
+                $sender->sendMessage("§cYou do not have permission to use this command!");
+                return false;
+            }
+            $sender->setGamemode(GameMode::CREATIVE());
+            $sender->sendMessage("§6§lBetterGameMode §r§8» §aYour gamemode has been changed to Survival");
+            return false;
+        }
+        
+        return true;
+    }
+}
