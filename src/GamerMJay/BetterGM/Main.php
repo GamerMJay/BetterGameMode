46ac949f62c2cbffc2e69572b1216d339c8e4399
GamerMJay <66413999+GamerMJay2008@users.noreply.github.com>
2022-01-28 01:28:41 +0100

    Update Main.php
    Now?

--- src/GamerMJay/BetterGM/Main.php
+++ src/GamerMJay/BetterGM/Main.php
@@ -2,13 +2,21 @@
 
 namespace GamerMJay\BetterGM;
 
+#commands
 use GamerMJay\BetterGM\commands\gm0;
 use GamerMJay\BetterGM\commands\gm1;
 use GamerMJay\BetterGM\commands\gm2;
 use GamerMJay\BetterGM\commands\gm3;
-use pocketmine\plugin\PluginBase;
-use pocketmine\Server;
 
+#pocketmine
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\tile\Tile;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\Server;
+use pocketmine\Player;
 
 class Main extends PluginBase {
 
