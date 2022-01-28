99c5602fc4332092e11c993f4a765d0e68f84f7b
GamerMJay <66413999+GamerMJay2008@users.noreply.github.com>
2022-01-28 01:41:03 +0100

    Update Main.php

--- src/GamerMJay/BetterGM/Main.php
+++ src/GamerMJay/BetterGM/Main.php
@@ -22,10 +22,5 @@ class Main extends PluginBase {
 
     public function onEnable(): void {
 
-	$this->getServer()->getCommandMap()->register("gm0", new gm0($this));
-        $this->getServer()->getCommandMap()->register("gm1", new gm1($this));
-	$this->getServer()->getCommandMap()->register("gm2", new gm2($this));
-        $this->getServer()->getCommandMap()->register("gm3", new gm3($this));
-
     }
 }
