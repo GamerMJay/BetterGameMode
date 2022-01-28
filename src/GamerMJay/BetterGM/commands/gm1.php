bfad13c009c5f3d706f624e73b79297584ed140b
GamerMJay <66413999+GamerMJay2008@users.noreply.github.com>
2022-01-28 01:12:58 +0100

    Update gm1.php
    Added use pocketmine\utils\Config;

--- src/GamerMJay/BetterGM/commands/gm1.php
+++ src/GamerMJay/BetterGM/commands/gm1.php
@@ -8,6 +8,7 @@ use pocketmine\command\CommandSender;
 use pocketmine\player\GameMode;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 
 class gm1 extends PluginBase
 {
